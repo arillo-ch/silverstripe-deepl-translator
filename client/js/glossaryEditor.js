9178a423704ec68698123c59fa493ca24bbed12b
Bumbus <sf@arillo.ch>
2023-08-08 13:47:29 +0200

    use arillo/silverstripe-cms-alpine repo & tidy up

--- client/js/glossaryEditor.js
+++ client/js/glossaryEditor.js
@@ -1,16 +1,8 @@
 /* global Alpine */
-// import Alpine from 'alpinejs';
+
 import deeplGlossary from './glossary';
 
-// (function ($) {
-//   if (typeof window.Alpine === 'undefined') {
-//     window.Alpine = Alpine;
-//   }
-//   Alpine.data('deeplGlossary', deeplGlossary);
-//   Alpine.start();
-// })(window.jQuery);
-
 document.addEventListener('alpine:init', () => {
-  console.log('alpine:init');
+  // console.log('alpine:init');
   Alpine.data('deeplGlossary', deeplGlossary);
 });
